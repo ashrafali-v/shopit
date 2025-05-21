14fa55d76677551fd1fc35c5c87b01668fde07bc
ashrafali-v <ashrafalifrk@gmail.com>
2025-05-21 22:25:55 +0800

    add dev tools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
+import { devtools } from "vue";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
     "@pinia/nuxt",
     "@nuxtjs/tailwindcss"
   ],
+  devtools:{enabled:true},
   nitro: {
     compressPublicAssets: true,
     compatibilityDate: "2025-05-15"
